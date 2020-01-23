2b598f6fe919ccf2f6d371cb6206de888084dccc
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-23 19:17:24 +0900

    주석 추가

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import firebase from "./Config/Config";
 import { LVRemove } from "./ListViewRemove";
 
+//수신 Collection 지정
 const db = firebase.firestore();
 const dbRef = db.collection("Item");
 
+//List View randering Function
 function ListView() {
   const [itemInfo, setItemInfo] = useState([]);
 
+  //Collection Item문서 수신
   useEffect(() => {
     const fetchData = () => {
       dbRef.onSnapshot(snapshot => {
@@ -15,6 +18,7 @@ function ListView() {
           id: doc.id,
           ...doc.data()
         }));
+        // Collection Item doc Data를 itemInfo에 반영
         setItemInfo(info);
       });
     };
@@ -23,16 +27,15 @@ function ListView() {
 
   return (
     <tbody>
-      {itemInfo.map((item, index) => (
-        <tr key={index}>
+      {itemInfo.map(item => (
+        <tr key={item.id}>
           <th scope="row">{item.type}</th>
           <td>{new Date(item.createAt.seconds * 1000).toLocaleString("ko")}</td>
           <td>{item.date}</td>
           <td>{item.itemNm}</td>
           <td>{item.lot}</td>
           <td>
-            {item.quantity}
-            {item.unit}
+            {item.quantity} {item.unit}
           </td>
           <td>{item.price}</td>
           <td>{item.registor}</td>
