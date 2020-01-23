310c392fde71144347ee796de3bb4b844949f13d
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-23 18:03:54 +0900

    ListView에서 Remove컴포넌트 분리

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,348 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebase from "./Config/Config";
+import { LVRemove } from "./ListViewRemove";
+
+const db = firebase.firestore();
+const dbRef = db.collection("Item");
 
 function ListView() {
+  const [itemInfo, setItemInfo] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      dbRef.onSnapshot(snapshot => {
+        const info = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setItemInfo(info);
+      });
+    };
+    return fetchData();
+  }, []);
+
   return (
     <tbody>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">Tools</th>
-        <td>2019.12.03</td>
-        <td>2019.12.01</td>
-        <td>고무망치</td>
-        <td>none</td>
-        <td>5 EA</td>
-        <td>50,000원</td>
-        <td>구매담당자</td>
-        <td>없음</td>
-        <td>
-          <button className="btn btn-outline-warning">수 정</button>
-        </td>
-        <td>
-          <button className="btn btn-outline-danger">삭 제</button>
-        </td>
-      </tr>
+      {itemInfo.map((item, index) => (
+        <tr key={index}>
+          <th scope="row">{item.type}</th>
+          <td>
+            {new Date(item.createAt.seconds * 1000).toLocaleDateString("ko")}
+          </td>
+          <td>{item.date}</td>
+          <td>{item.itemNm}</td>
+          <td>{item.lot}</td>
+          <td>
+            {item.quantity}
+            {item.unit}
+          </td>
+          <td>{item.price}</td>
+          <td>{item.registor}</td>
+          <td>{item.etc}</td>
+          <td>
+            <button className="btn btn-outline-warning">수 정</button>
+          </td>
+          <td>
+            <LVRemove item={item} />
+          </td>
+        </tr>
+      ))}
     </tbody>
   );
 }
---
+++ src/components/ListViewRemove.js
@@ -0,0 +1,23 @@
+import React from "react";
+import firebase from "./Config/Config";
+
+const db = firebase.firestore();
+const dbRef = db.collection("Item");
+export const LVRemove = ({ item }) => {
+  const handleRemove = () => {
+    try {
+      alert("Data Remove?");
+      dbRef.doc(item.id).delete();
+      console.log("Data Remove");
+    } catch (error) {
+      console.error("Remove Failed", error);
+      alert("Data Failed");
+    }
+  };
+
+  return (
+    <button className="btn btn-outline-danger" onClick={handleRemove}>
+      삭 제
+    </button>
+  );
+};
