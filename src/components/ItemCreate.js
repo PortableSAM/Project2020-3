c80363319374635ddccce5a5ef247ab0f9cabf8f
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-27 15:53:22 +0900

    주석 변경

--- src/components/ItemCreate.js
+++ src/components/ItemCreate.js
@@ -6,9 +6,11 @@ import "./Style/ItemCreate.css";
 //fireStore collection 지정(doc.id는 자동생성).
 const db = firebase.firestore();
 const dbRef = db.collection("Item").doc();
+//fireStore Timestamp 적용.
 const createAt = firebase.firestore.Timestamp.fromDate(new Date());
 
 export const ItemInput = () => {
+  //React Hook Form 사용.
   const { register, handleSubmit, errors } = useForm();
 
   const onSubmit = data => {
@@ -26,70 +28,54 @@ export const ItemInput = () => {
         <h4>물품등록</h4>
       </div>
       <form className="input_form" onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          작성자
+        <input
+          type="text"
+          name="registor"
+          placeholder="작성자"
+          ref={register({ required: true })}
+        />
+        {errors.registor && `작성자 입력요망`}
+        <div className="item_name">
           <input
             type="text"
-            name="registor"
+            name="itemNm"
+            placeholder="제품이름"
             ref={register({ required: true })}
           />
-          {errors.registor && `작성자 입력요망`}
-        </label>
-        <div className="item_name">
-          <label>
-            제품이름
-            <input
-              type="text"
-              name="itemNm"
-              ref={register({ required: true })}
-            />
-            {errors.itemNm && `제품이름 입력요망`}
-          </label>
-          <label>
-            분류
-            <select type="text" name="type" ref={register}>
-              <option value="Mask">Mask</option>
-              <option value="Tool">Tool</option>
-              <option value="Gloves">Gloves</option>
-              <option value="Office">Office</option>
-              <option value="Computer goods">Computer goods</option>
-              <option value="etc">etc.</option>
-            </select>
-          </label>
+          {errors.itemNm && `제품이름 입력요망`}
+          <select type="text" name="type" ref={register}>
+            <option value="Mask">Mask</option>
+            <option value="Tool">Tool</option>
+            <option value="Gloves">Gloves</option>
+            <option value="Office">Office</option>
+            <option value="Computer goods">Computer goods</option>
+            <option value="etc">etc.</option>
+          </select>
         </div>
-        <label>
-          로트번호
-          <input type="text" name="lot" ref={register} />
-        </label>
+        <input type="text" name="lot" placeholder="로트번호" ref={register} />
         <div className="item_quanti">
-          <label>
-            수량
-            <input
-              type="number"
-              name="quantity"
-              min="0"
-              ref={register({ required: true })}
-            />
-            {errors.quantity && `수량 입력요망`}
-          </label>
-          <label>
-            단위
-            <select type="text" name="unit" ref={register}>
-              <option value="EA">EA</option>
-              <option value="SET">SET</option>
-              <option value="PK">PK</option>
-            </select>
-          </label>
+          <input
+            type="number"
+            name="quantity"
+            min="0"
+            placeholder="수량"
+            ref={register({ required: true })}
+          />
+          {errors.quantity && `수량 입력요망`}
+          <select type="text" name="unit" ref={register}>
+            <option value="EA">EA</option>
+            <option value="SET">SET</option>
+            <option value="PK">PK</option>
+          </select>
         </div>
-        <label>
-          구매가격
-          <input type="text" name="price" ref={register} />
-        </label>
-        <label>
-          구입일자
-          <input type="date" name="date" ref={register({ required: true })} />
-          {errors.date && `구입일자 입력요망`}
-        </label>
+        <input type="text" name="price" placeholder="구입금액" ref={register} />
+        <input
+          type="date"
+          name="date"
+          placeholder="구입일자"
+          ref={register({ required: true })}
+        />
+        {errors.date && `구입일자 입력요망`}
         <textarea
           name="etc"
           placeholder="기타 특이사항 및 상세사항 기입."
